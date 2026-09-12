cefe70dd1200849b53be26391606a371002ced77
“John <john@email.com>
2026-09-12 16:00:41 +0300

    fixed tests for model

--- app/project/tests/test_models.py
+++ app/project/tests/test_models.py
@@ -1,10 +1,9 @@
 from models import Item, ShoppingCart
 
-apple =Item(name="apple", price=10, quantity=1)
-orange = Item(name="orange", price=20, quantity=1)
-
 class TestShoppingCart:
+
     def test_adding_item(self):
+        apple = Item(name="apple", price=10, quantity=1)
         shopping_cart = ShoppingCart()
         shopping_cart.add_item(apple)
         assert shopping_cart.items[0].name == "apple"
@@ -12,12 +11,15 @@ class TestShoppingCart:
         assert shopping_cart.items[0].quantity == 1
 
     def test_removing_item(self):
+        apple = Item(name="apple", price=10, quantity=1)
         shopping_cart = ShoppingCart()
         shopping_cart.add_item(apple)
         shopping_cart.remove_item(apple)
         assert len(shopping_cart.items) == 0
 
     def test_get_total(self):
+        apple = Item(name="apple", price=10, quantity=1)
+        orange = Item(name="orange", price=20, quantity=1)
         shopping_cart = ShoppingCart()
         shopping_cart.add_item(apple)
         shopping_cart.add_item(orange)
