8dd25b6c50f9d8fc88558a20d3595f83d88d4933
“John <john@email.com>
2026-09-12 12:46:29 +0300

    added tests for model

--- app/project/models.py
+++ app/project/models.py
@@ -8,15 +8,20 @@ class ShoppingCart():
     def __init__(self):
         self.items = []
 
-    def add_item(self, item: Item):
+    def add_item(self, item: Item) -> None:
         for existing_item in self.items:
             if existing_item.name == item.name:
                 existing_item.quantity += item.quantity
                 existing_item.price += item.price
-                print(self.items)            # fix later
                 return
-        self.items.append(item)
-        print(self.items)                    # fix later
-shopping_cart = ShoppingCart()
-shopping_cart.add_item(Item("Breakfast", 5.00, 1))
 
+        self.items.append(item)
+
+    def remove_item(self, item: Item) -> None:
+        if item in self.items:
+            self.items.remove(item)
+
+    def get_total(self, total_price: float = 0) -> float:
+        for item in self.items:
+            total_price += item.price * item.quantity
+        return total_price
